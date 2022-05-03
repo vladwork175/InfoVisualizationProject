5e268ad3b188442fa7a6a4f1b4d0fcddf9fd075a
vladwork175 <70330664+vladwork175@users.noreply.github.com>
2022-05-02 23:36:35 -0400

    Update carbsindex.js
    Svg expansion

--- carbsindex.js
+++ carbsindex.js
@@ -1,6 +1,6 @@
 var margin = { top: 20, right: 20, bottom: 30, left: 30 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        width = 1250 - margin.left - margin.right,
+        height = 550 - margin.top - margin.bottom;
 
 var x = d3.scaleLinear()
         .range([0, width - 150]);
